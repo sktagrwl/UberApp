01bdab4838efa1256701e3a17902a480a95c42ef
sktagrwl <agarrwaldevansh23@gmail.com>
2026-06-21 16:49:07 +0530

    Initial commit: Project Setup and MySQL connection

---
+++ src/test/java/com/example/UberApp/UberAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UberApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
